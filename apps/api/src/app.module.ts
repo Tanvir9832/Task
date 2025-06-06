950a18e118eb25260115866a241e90f591b0336c
Md. Tanvir Alam Anik <>
2025-06-07 00:00:57 +0600

    feat: add users

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
